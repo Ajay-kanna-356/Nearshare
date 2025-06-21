7b68fa3a413b320da71e376dfb77f86056e3e20a
Kishore-Babu-K <kishorebabu.kishore10@gmail.com>
2025-06-22 01:11:20 +0530

    MongoDB Is Connected.

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,28 @@
 const express = require("express");
+const connectDB = require('./config/db');
+const mongoose = require('mongoose');
 const app = express();
+
+connectDB();
+
+const User = require('./models/userModel');
+
+
+mongoose.connection.once('open', async () => {
+  try {
+    const newUser = await User.create({
+      name: 'Stark',
+      phoneNum: '1234567890',
+      emailId: 'stark@example.com',
+      password: 'passwordisnopassword'
+    });
+
+    console.log('Inserted User:', newUser);
+  } catch (error) {
+    console.error('Error inserting user:', error.message);
+  }
+});
+
 
 app.get("/",(req,res) =>{
     res.send("Testing....");
