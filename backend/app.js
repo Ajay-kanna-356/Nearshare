5ac9af665ebc52685dd41078bcb18e394f50823d
Ajay-kanna-356 <ajaykanna356@gmail.com>
2025-06-25 21:38:23 +0530

    added login and register page

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
-
-app.get("/",(req,res) =>{
-    res.send("Testing....");
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));  
+app.get("/",(req,res) =>{    
+    res.sendFile(__dirname+"/public/login.html");
 })
-
+app.post("/",(req,res) =>{
+    const {username,password} = req.body;
+    console.log(username,password);
+    res.sendFile(__dirname+"/public/home.html")
+})
+app.get("/register",(req,res) =>{
+    res.sendFile(__dirname+"/public/register.html")
+})
+app.post("/register",(req,res) =>{
+    const {name,phone,email,password,cpassword} = req.body;
+    console.log(name,phone,email,password,cpassword);
+    if(password === cpassword){
+    res.sendFile(__dirname+"/public/home.html")
+    }
+    else{
+    res.sendFile(__dirname+"/public/register.html")
+    }
+})
 app.listen(2000,() =>{
     console.log("server running in 2000");
 })
