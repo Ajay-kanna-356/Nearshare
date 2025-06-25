08f0a484f0178181677569a565e761e1b84f2891
Kishore-Babu-K <kishorebabu.kishore10@gmail.com>
2025-06-26 00:29:15 +0530

    Implemented login and register with MongoDB integration

--- backend/app.js
+++ backend/app.js
@@ -25,25 +25,69 @@ mongoose.connection.once('open', async () => {
  
 app.get("/",(req,res) =>{    
     res.sendFile(__dirname+"/public/login.html");
-})
-app.post("/",(req,res) =>{
-    const {username,password} = req.body;
-    console.log(username,password);
-    res.sendFile(__dirname+"/public/home.html")
-})
+});
+
+
+app.post("/",async(req,res) =>{
+    const {email,password} = req.body;
+    console.log(email,password);
+    try {
+    // Find user by their username OR email — depends on your schema
+    const user = await User.findOne({ emailId: email }); 
+    
+    // Finding Email-Id
+    if (!user) {
+      return res.status(400).send('User not found.');
+    }
+    // Password Check 
+    if (user.password !== password) {
+      return res.status(400).send('Incorrect password.'); // For now it is Showing As Error Message Further Will Be Enhanced
+    }
+
+    //Successful login
+    res.sendFile(__dirname + "/public/home.html");
+
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).send('Database Server Error, please try again.');
+  }
+});
+
 app.get("/register",(req,res) =>{
     res.sendFile(__dirname+"/public/register.html")
 })
-app.post("/register",(req,res) =>{
+app.post("/register",async(req,res) =>{
     const {name,phone,email,password,cpassword} = req.body;
+    
     console.log(name,phone,email,password,cpassword);
-    if(password === cpassword){
-    res.sendFile(__dirname+"/public/home.html")
+    
+    // Confirm password Check 
+    if(password === cpassword){ 
+     try {
+    // Inserting User Into MongoDB
+    const newUser = await User.create({
+      name: name,
+      phoneNum: phone,
+      emailId: email,
+      password: password
+    });
+
+    console.log('Inserted user:', newUser);
+    res.sendFile(__dirname + "/public/login.html");
+  } catch (error) {
+    console.error('Error inserting user:', error.message);
+    res.status(500).send('Error occurred, please try again.');
+  }
     }
+
     else{
+    res.status(400).send(("Password Does Not Match Confirm Password"))
     res.sendFile(__dirname+"/public/register.html")
     }
-})
+
+    
+});
+
 app.listen(2000,() =>{
     console.log("server running in 2000");
 })
